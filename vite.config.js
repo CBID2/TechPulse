4b8c65b4655090b02a5345bacf43db79ae5b11b8
Aditya <adityasahoo652@gmail.com>
2025-03-18 23:30:26 +0530

    Add gzip compression plugin to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import compression from 'vite-plugin-compress';
 
 export default defineConfig(() => {
   return {
     build: {
       outDir: 'dist',
     },
-    plugins: [react()],
+    plugins: [react(),compression({ algorithm: 'gzip' })],
   };
 });
